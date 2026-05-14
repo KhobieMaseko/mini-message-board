82af6eeb198e94949f229b0832bbe1e5f9669cb6
Khobie Maseko <88542655+KhobieMaseko@users.noreply.github.com>
2026-05-14 14:07:12 +0400

    Fix production PostgreSQL connection using DATABASE_URL

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,20 @@
 require("dotenv").config();
+// const { Pool } = require("pg");
+
+// module.exports = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+module.exports = pool;
